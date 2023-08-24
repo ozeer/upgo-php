cbb5522a8084b76a8df2bbb5aba5ff071a092733
zhouyang <zhouyang2021@163.com>
2023-08-24 11:13:03 +0800

    add files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,9 @@
 
 declare(strict_types=1);
 
-require 'vendor/autoload.php';
+require './vendor/autoload.php';
 
+date_default_timezone_set('Asia/Shanghai');
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
